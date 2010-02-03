74fa7063f95997f0aa98d234220e861a18c9fbd5
Kristian Rosenvold <kristian@rosenvold.com>
2010-02-03 21:46:41 +0100

    FIxed

--- src/main/java/org/jdogma/concurrency/ThreadAccessController.java
+++ src/main/java/org/jdogma/concurrency/ThreadAccessController.java
@@ -7,20 +7,36 @@ import java.util.Set;
 
 public class ThreadAccessController implements Serializable {
     final Set<Thread> seenThreads = Collections.synchronizedSet(new HashSet<Thread>());
+    final StackTraceElement stackTraceAtConstruction;
     Thread lastThread;
 
     public ThreadAccessController() {
+        try
+        {
+            throw new Exception();
+        }
+        catch ( Exception e )
+        {
+            stackTraceAtConstruction = e.getStackTrace()[2];
+        }
     }
 
     public void checkThreadAccess() {
         final Thread thread = Thread.currentThread();
         if (seenThreads.contains(thread)) {
             if (!thread.equals(lastThread)) {
-                throw new UnsafeCollectionAccessException("The thread is accessing this collection intertwined with others");
+                throw new UnsafeCollectionAccessException("The thread is accessing this collection intertwined with others, constructed at:" + getStackTrace());
             }
         } else {
             seenThreads.add(thread);
         }
         lastThread = thread;
+    }
+
+    private String getStackTrace(){
+        StringBuilder result = new StringBuilder( );
+        result.append(stackTraceAtConstruction.toString());
+        result.append( "\n");
+        return result.toString();
     }
 }
